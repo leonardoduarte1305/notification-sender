5f3b0fcf923c7b5dd1185cd164bb5347cd8ad47e
leonardosilva <leonardoduarte1305@gmail.com>
2025-02-19 09:33:46 -0300

    notification-sender-service created

---
+++ notification-sender-service/src/test/java/br/dev/notificationsender/NotificationSenderTestApplication.java
@@ -0,0 +1,11 @@
+package br.dev.notificationsender;
+
+import org.springframework.boot.SpringApplication;
+
+class NotificationSenderTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationSenderApplication.class, args);
+    }
+
+}
