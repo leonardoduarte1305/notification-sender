965c3eed41481ec85e6dfc1b7d21bd8b197a8044
leonardosilva <leonardoduarte1305@gmail.com>
2026-06-25 16:54:39 -0300

    7 - Alterado o Converter do consumer para StringJacksonJsonMessageConverter

--- src/main/java/br/dev/notificationsender/configuration/CommonBeans.java
+++ src/main/java/br/dev/notificationsender/configuration/CommonBeans.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJacksonJsonMessageConverter;
 
 @Configuration
 public class CommonBeans {
@@ -11,6 +13,11 @@ public class CommonBeans {
     @Bean
     public Module javaTimeModule() {
         return new JavaTimeModule();
+    }
+
+    @Bean
+    public RecordMessageConverter kafkaRecordMessageConverter() {
+        return new StringJacksonJsonMessageConverter();
     }
 
 }
